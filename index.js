7e62a863e0c2619160346833ada4395531cf3fb0
Pavlo Bashynskyi <levonet@gmail.com>
2021-04-13 22:25:46 +0300

    Create comment parser prototype (#1)

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const core = require('@actions/core')
+const { context } = require('@actions/github')
 
 async function run() {
     try {
-        core.info('TBD')
+        core.info(`eventName: ${context.eventName}`)
+        core.info(JSON.stringify(context.payload))
+        if (context.eventName === 'issue_comment' && !context.payload.issue.pull_request) {
+            core.info('There is no comment action.')
+            return
+        }
+
+        const expect = core.getInput('expect', {required: true})
+        const body = context.eventName === 'issue_comment'
+            ? context.payload.comment.body
+            : context.payload.pull_request.body
+
+        core.info(`expect: ${expect}`)
+
+        core.setOutput('body', body)
+        core.info(`body: ${body}`)
     } catch (error) {
         core.setFailed(error.message)
     }
